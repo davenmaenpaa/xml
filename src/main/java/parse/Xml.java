5384c20010de9772b8a70b64d6d0b593686ab30e
Daven Mäenpää <daven.maenpaa@outlook.com>
2018-06-16 19:07:05 +0200

    comments and format

--- src/main/java/parse/Xml.java
+++ src/main/java/parse/Xml.java
@@ -9,45 +9,56 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Generic XML to object- and object to XML-converter.
  */
 public final class Xml {
     private final Logger LOGGER = Logger.getLogger(Xml.class.getName());
 
-    public Object parseFrom(String path, Class<?> c) {
-        Object order = null;
+    /**
+     * Returns a generic object from a XML-file.
+     *
+     * @param path  path to the XML-file to convert from
+     * @param type  object type to derive from the XML-file
+     * @return      object obtained from the XML-file
+     */
+    public Object toObject(String path, Class<?> type) {
+        Object object = null;
 
         try {
 
             File file = new File(path);
+            JAXBContext jaxbContext = JAXBContext.newInstance(type);
 
-            JAXBContext jaxbContext = JAXBContext.newInstance(c);
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-
-            order = (c.cast(jaxbUnmarshaller.unmarshal(file)));
+            object = jaxbUnmarshaller.unmarshal(file);
 
         } catch (JAXBException e) {
             LOGGER.log(Level.SEVERE, e.toString());
         }
 
-        return order;
+        return object;
     }
-
-
-    public void parseTo(Object object, String path) {
+    
+    /**
+     * Converts a generic object to a file in XML-format and prints the result in the console.
+     *
+     * @param object    object to be converted
+     * @param path      output path for the file
+     * @param <T>       type of the object to convert
+     */
+    public <T> void toFile(String path, T object) {
         try {
 
             File file = new File(path);
-
             JAXBContext jaxbContext = JAXBContext.newInstance(object.getClass());
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
 
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             jaxbMarshaller.marshal(object, file);
             jaxbMarshaller.marshal(object, System.out);
 
         } catch (JAXBException e) {
-            LOGGER.log(Level.SEVERE,e.toString());
+            LOGGER.log(Level.SEVERE, e.toString());
         }
     }
 }
