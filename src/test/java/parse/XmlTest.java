2620e682c95e4a5bed7f3d7a61ff6bc4a33e59c5
Daven Mäenpää <daven.maenpaa@outlook.com>
2018-06-18 15:57:09 +0200

    format

--- src/test/java/parse/XmlTest.java
+++ src/test/java/parse/XmlTest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.file.Files;
-
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
